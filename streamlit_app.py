e154f51e7b10418d6b74f6aa6491c6570e53fbf6
Amar <107593984+Interioamar@users.noreply.github.com>
2023-04-04 11:29:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,6 @@ st.text('😀 ale, Spinach & Rocket Smoothie')
 st.text('🍐 Like a Hard-Boiled Free-Range Egg')
 
 st.header('🍌🍋 Build Your Own Fruit Smoothie  🍇🐔')
+
+list1=pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+st.dataframe(list1)
