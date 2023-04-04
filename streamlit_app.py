d216e94096711a5236751de99450161bfb7748cc
Amar <107593984+Interioamar@users.noreply.github.com>
2023-04-04 11:13:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,11 @@
 #this is the main file
 import streamlit as st
 import numpy as np
+import pandas as pd
 st.title("Hoorah the application is deployed")
 
 st.write("Let's list few more")
-dataframe = np.random.randn(10, 20)
-st.dataframe(dataframe)
-
+print("/n")
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
