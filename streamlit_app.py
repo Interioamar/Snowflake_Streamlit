c80ad40c164929b13e93ab81152f3d7d33181308
Amar <107593984+Interioamar@users.noreply.github.com>
2023-04-04 11:12:14 +0530

    Added few streamlit functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 #this is the main file
-import streamlit
-streamlit.title("Hoorah the application is deployed")
+import streamlit as st
+import numpy as np
+st.title("Hoorah the application is deployed")
+
+st.write("Let's list few more")
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
