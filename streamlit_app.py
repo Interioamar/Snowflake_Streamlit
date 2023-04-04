ed042720a8a05cd2430fe84a2a14b65da54a70dc
Amar <107593984+Interioamar@users.noreply.github.com>
2023-04-04 11:16:25 +0530

    Added few streamlit functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,6 @@ import numpy as np
 import pandas as pd
 st.title("Hoorah the application is deployed")
 
-st.write("Let's list few more")
-print("/n")
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
+st.header("Breakfast Menu")
+st.text("Soup")
+st.text("Boiled Egg")
