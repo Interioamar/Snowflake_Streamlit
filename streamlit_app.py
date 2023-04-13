bc0acd37b8afe48ee6c518699c493898d36f0b0e
Amar <107593984+Interioamar@users.noreply.github.com>
2023-04-13 21:31:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,14 +39,17 @@ import streamlit
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from_streamlit')")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
+
 st.text("The fruit load list contains")
 st.dataframe(my_data_row)
-
 fruit_choice = st.text_input('What fruit would you like to add','jackfruit') #creates an input box for fruits with default value as kiwi
 st.write('Thanks for adding ', fruit_choice) #creates an text with The user entered Kiwi/input
+
+
+
 
 
 
